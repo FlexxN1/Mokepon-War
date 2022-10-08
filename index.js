3243379e62431beb414f6cdb0f1e7d5af960b666
Flexx <juandavidx40@gmail.com>
2022-10-08 15:43:09 -0500

    Add JSON and POST index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+
 const players = [];
 
 class Player {
     constructor(id){
         this.id = id;
     }
+
+    asignarMokepon(mokepon){
+        this.mokepon = mokepon
+    }
 }
+
+class Mokepon {
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+}
+
 app.get("/join", (req, res) => {
     const id = `${Math.random()}`;
     const player = new Player(id)
@@ -16,6 +32,21 @@ app.get("/join", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
 
     res.send(id)
+});
+
+app.post("/mokepon/:playerId", (req, res) => {
+    const playerId = req.params.playerId || "";
+    const nombre = req.body.mokepon || "";
+    const mokepon = new Mokepon(nombre);
+    
+    const jugadorIndex = players.findIndex((player) => playerId === player.id);
+    if(jugadorIndex >= 0){
+        players[jugadorIndex].asignarMokepon(mokepon);
+    }
+
+    console.log(players);
+    console.log(playerId)
+    res.end()
 });
 
 app.listen(8080, () => {
